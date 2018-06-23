13754fc64043bce0398df4de6a2ca513da775a2a
Ilter Kose <ilterkose98@icloud.com>
2018-06-24 00:51:58 +0300

    problem2 24.06

--- problem2.py
+++ problem2.py
@@ -1,10 +1,28 @@
-a = 1
-b = 2
+a = 0
+b = 1
+
 fibonacci = [a,b]
 
-for i in range (4000000):
-
-    a,b = b, a+b
-    fibonacci.append(b)
+while a < 4000000:
+    print(a)
+    a, b = b, a+b
+    if  (b % 2) == 0:
+        fibonacci.append(b)
 
 print(sum(fibonacci))
+
+
+a = 0
+b = 1
+toplam = 0
+fibonacci = [a,b]
+
+while a < 4000000:
+    print(a)
+    a, b = b, a+b
+    if  (b % 2) == 0:
+        toplam += b
+
+print((toplam))
+
+
